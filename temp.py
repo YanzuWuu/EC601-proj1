19e8e74845b5bc24f1388244b187ac7c62199035
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2018-09-17 09:55:39 +0800

    Update temp.py
    download pictures in the same-path-file

--- temp.py
+++ temp.py
@@ -1,19 +1,19 @@
-
 #!/usr/bin/env python
 # encoding: utf-8
 #Author - Prateek Mehta
 
 
 import tweepy #https://github.com/tweepy/tweepy
-import json
-
-
+#import json
+import urllib.request
+import os
+import wget
 #Twitter API credentials
+
 consumer_key = 'P0ZYeebu0IiZ2yXDkJYMKa4QN'
 consumer_secret = '1o8drgPRXHxks3FhGLeNJbMXjPf8p3GbE0ywbdlRS1kpGEU5LO'
-access_key = '1037401110389174272-6iC08TC3wyhtB3QE5lzaipeM18KYe7'
+access_token = '1037401110389174272-6iC08TC3wyhtB3QE5lzaipeM18KYe7'
 access_secret = 'TkxumYnCOj8zepySPctzFeKvDS9SFx82gKVjS6evfOjMe'
-
 
 def get_all_tweets(screen_name):
     
@@ -21,7 +21,7 @@ def get_all_tweets(screen_name):
     
     #authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
+    auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
     
     #initialize a list to hold all the tweepy Tweets
@@ -47,21 +47,53 @@ def get_all_tweets(screen_name):
         
         #update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        if(len(alltweets) > 20):
+        if(len(alltweets) > 15):
             break
         print ("...%s tweets downloaded so far" % (len(alltweets)))
        
     #write tweet objects to JSON
     file = open('tweet.json', 'w') 
+
+    #write tweet objects to JSON
+    file = open('tweet.json', 'w') 
     print ("Writing tweet objects to JSON please wait...")
+    media_files=set()
     for status in alltweets:
-        #json.dump(status._json,file,sort_keys = True,indent = 4)
         media = status.entities.get('media',[])
-        print(media)
-    #close the file
-    print ("Done")
+        if (len(media)>0):
+            media_files.add(media[0]['media_url'])
+    
+    for media_file in media_files:
+        wget.download(media_file)
     file.close()
+    
+'''    
+def save_img(media_url,file_name,file_path='pic'):
+    #save in pic
+    try:
+        if not os.path.exists(file_path):
+            print ('folder',file_path,'donot exist, create again')
+            #os.mkdir(file_path)
+            os.makedirs(file_path)
+        #attain the last word of pics
+
+       
+
+        file_suffix = os.path.splitext(media_url)[1]
+        #full file name
+        filename = '{}{}{}{}'.format(file_path,os.sep,file_name,file_suffix)
+        #save pics in file
+        wget.download(media_url,file_path)
+    except IOError as e:
+        print ('process fault',e)
+    except Exception as e:
+        print ('erro：',e)
+'''
+#close the file
+
 
 if __name__ == '__main__':
     #pass in the username of the account you want to download
     get_all_tweets("@Ibra_official")
+#    media_url = get_all_tweets("@Ibra_official")
+#    save_img(media_url,file_name,file_path='pic')
